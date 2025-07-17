e19d22c7f6897c595b688176399032fc19c48e98
Montassar-Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2025-07-17 11:14:24 +0100

    refactor: made a dynamic route instead of static one

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
-def index(request):
-  return HttpResponse("This Works!")
+def monthly_challenge(request, month):
+  challenge_text = None
+  if month == 'january':
+    challenge_text = 'Eat 500g proteins every day!'
+  elif month == 'february':
+    challenge_text = 'Learn how to fish!'
+  elif month == 'march':
+    challenge_text = 'Become a Django Developer!'
+  else :
+    return HttpResponseNotFound("This month is not supported")
+  
+  return HttpResponse(challenge_text)
