28863f9a3038e8e9fb9d27fb1e94080b7d434f50
Montassar-Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2025-07-17 13:34:51 +0100

    feat(redirect): now u can use numbers and the website will redirect u to the month

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound,HttpResponseRedirect
 
 monthly_challenges = {
   'january': 'Eat 500g proteins every day!',
@@ -10,19 +10,22 @@ monthly_challenges = {
   'june': 'Run 100 kilometers in total!',
   'july': 'Launch a personal blog!',
   'august': 'No sugar for 30 days!',
-  'september': 'Master the basics of French!',
+  'september': 'Master the basics of Spanish!',
   'october': 'Build and deploy a mobile app!',
   'november': 'Practice meditation daily!',
   'december': 'Reflect and write your yearly review!'
 }
 
-
 def monthly_challenge_by_number(request, month):
-  return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    if month > 12 or month < 1:
+     return HttpResponseNotFound('Invalid Month!')
+    redirect_month = months[month-1]
+    return HttpResponseRedirect('/challenges/' + redirect_month)
 
 def monthly_challenge(request, month):
   try:
-    challenge_text = monthly_challenge[month]
+    challenge_text = monthly_challenges[month]
   except:
     return HttpResponseNotFound('This month is not supported!')
   return HttpResponse(challenge_text)
