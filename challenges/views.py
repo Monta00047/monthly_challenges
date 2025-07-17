cd3e708de3f852eabc6df7f790d3ffdb56784fb6
Montassar-Benelkhouja <montassarbenelkhouja@takiacademyteam.com>
2025-07-17 15:09:59 +0100

    added a challenges page wich contains a list of all months

--- challenges/views.py
+++ challenges/views.py
@@ -17,10 +17,20 @@ monthly_challenges = {
   'december': 'Reflect and write your yearly review!'
 }
 
+def index(request):
+  list_items=''
+  months = list(monthly_challenges.keys())
+  for month in months:
+    month_path = reverse("month-challenge", args=[month])
+    list_items += f"<li> <a href='{month_path}'>{month.capitalize()}</a> </li>"
+
+  response_data = f"<ul>{list_items}</ul>"
+  return HttpResponse(response_data)
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > 12 or month < 1:
-     return HttpResponseNotFound('Invalid Month!')
+     return HttpResponseNotFound('<h1>Invalid Month!</h1>')
     redirect_month = months[month-1]
     redirect_path = reverse('month-challenge', args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
@@ -28,6 +38,7 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
+    response_data = f'<h1>{challenge_text}</h1>'
   except:
-    return HttpResponseNotFound('This month is not supported!')
-  return HttpResponse(challenge_text)
+    return HttpResponseNotFound('<h1>This month is not supported!</h1>')
+  return HttpResponse(response_data)
